e94bfb20036d65473bdf3fbcdbae2f24735b5e2c
bobrofon <bobrofon@gmail.com>
2018-09-06 19:20:32 +0700

    Try wine for travis

--- modules/test_runner.cmake
+++ modules/test_runner.cmake
@@ -5,7 +5,7 @@ enable_testing()
 
 add_test(
         NAME ${TEST_RUNNER_NAME}
-        COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/test/${TEST_RUNNER_NAME} ${PROJECT_NAME}
+        COMMAND wine ${CMAKE_CURRENT_SOURCE_DIR}/test/${TEST_RUNNER_NAME} ${PROJECT_NAME}
 )
 
 set_tests_properties(${TEST_RUNNER_NAME} PROPERTIES DEPENDS ${PROJECT_NAME})
